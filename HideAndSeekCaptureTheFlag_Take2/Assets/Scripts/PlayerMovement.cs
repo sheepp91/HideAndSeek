c5a21233be4a832d1c09cf71138441c91764ec65
Joshua Laurence <joshua_laurence@live.com>
2020-07-09 23:12:38 +1000

    Player goes invisible in shadows

--- HideAndSeekCaptureTheFlag_Take2/Assets/Scripts/PlayerMovement.cs
+++ HideAndSeekCaptureTheFlag_Take2/Assets/Scripts/PlayerMovement.cs
@@ -4,22 +4,60 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    public Transform light;
+    public GameObject playerGraphics;
+
     public float speed = 5f;
 
-    private Rigidbody rb;
+    //private Rigidbody rb;
+    private MeshRenderer playerMesh;
+    private MeshRenderer[] playerChildMeshes;
 
     void Start()
     {
-        rb = GetComponent<Rigidbody>();
+        //rb = GetComponent<Rigidbody>();
+        playerMesh = playerGraphics.GetComponent<MeshRenderer>();
+        playerChildMeshes = playerGraphics.GetComponentsInChildren<MeshRenderer>();
     }
 
     void FixedUpdate()
     {
-        float moveHorizontal = Input.GetAxis("Horizontal");
-        float moveVertical = Input.GetAxis("Vertical");
+        //float moveHorizontal = Input.GetAxis("Horizontal");
+        //float moveVertical = Input.GetAxis("Vertical");
+        //
+        //Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        //
+        //rb.AddForce(movement * speed);
 
-        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        CheckInvisible();
+    }
 
-        rb.AddForce(movement * speed);
+    private void CheckInvisible()
+    {
+
+        Vector3 lightRot = light.forward;
+        Vector3 directionToLight = light.forward * -1;
+
+        RaycastHit hit;
+
+        Debug.DrawRay(transform.position, directionToLight, Color.red);
+
+        if (Physics.Raycast(transform.position, directionToLight, out hit, Mathf.Infinity))
+        {
+            MakeVisible(false);
+        }
+        else
+        {
+            MakeVisible(true);
+        }
+    }
+
+    private void MakeVisible(bool shouldBeVisble)
+    {
+        playerGraphics.GetComponent<MeshRenderer>().enabled = shouldBeVisble;
+        foreach (MeshRenderer mr in playerChildMeshes)
+        {
+            mr.enabled = shouldBeVisble;
+        }
     }
 }
